3b98f2a80728776e25c2d8963d0093667885736d
LilicaRepilica29 <livia59790516@aluno.es.senac.br>
2026-02-03 16:18:44 -0300

    biblioteca de arquivos

---
+++ teste.return.py
@@ -0,0 +1,32 @@
+def lin():
+    print("=" * 40)
+#usando return
+def somar(a, b):
+    resultado = a + b
+    return resultado
+soma = somar(5, 3)
+print(soma)
+lin()
+
+#Quando você não usa return
+def somar(a, b):
+    resultado = a + b
+    print(resultado)
+soma = somar(5, 3)
+print(soma)
+lin()
+
+#Função que só executa (print)
+def somar(a, b):
+    print(a + b)
+
+x = somar(5, 3)
+print("Valor em x:", x)
+lin()
+
+#Função que retorna
+def somar(a, b):
+    return a + b
+x = somar(5, 3)
+print("Valor em x:", x)
+lin()
