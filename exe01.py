4048ea7dbfeb161c66b2353eb2c2d686b90a8149
LilicaRepilica29 <livia59790516@aluno.es.senac.br>
2026-02-04 17:03:46 -0300

    mudança

---
+++ exe01.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número: "))
+if numero > 0 :
+    print("O número é Positivo!")
+elif numero < 0 :
+    print("O número é Negativo!")    
+else:
+    print("O número é Zero(0)")
