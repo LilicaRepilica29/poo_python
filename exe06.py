68663a1f4362e9bd46ef96d46e1b037b3a08ce03
LilicaRepilica29 <livia59790516@aluno.es.senac.br>
2026-02-04 16:18:58 -0300

    teste

---
+++ exe06.py
@@ -0,0 +1,7 @@
+nome1 = input("Digite 1º nome: ")
+nome2 = input("Digite 2º nome: ")
+nome3 = input("Digite 3º nome: ")
+nome4 = input("Digite 4º nome: ")
+nome5 = input("Digite 5º nome: ")
+nomes = [nome1,nome2,nome3,nome4,nome5]
+print(f"A quantidade de cadastros é: {len(nomes)}")
